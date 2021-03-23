3a7d5156cccb329ec05d6b79ca51f193207d36bc
LesMiserables1 <liemandre20@gmail.com>
2021-03-23 22:06:59 +0700

    change websocket to socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,30 @@
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 8080 });
-
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    ws.send(message)
-    console.log('received: %s', message);
-  });
-  // var stdin = process.openStdin();
-  // stdin.addListener("data", function(d) {
-  //     ws.send(d.toString().trim())
-  // });
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  
 });
 
+io.on("connection", (socket) => {
+  console.log('a user connected')
+  socket.on('message',(msg)=>{
+    socket.send('test')
+    console.log(msg)
+  })
+});
+
+httpServer.listen(3000);
+// const WebSocket = require('ws');
+
+// const wss = new WebSocket.Server({ port: 8080 });
+
+
+// wss.on('connection', function connection(ws) {
+//   ws.send('test')
+//   ws.on('message', function incoming(message) {
+//     ws.send(message)
+//     console.log('received: %s', message);
+//   });
+//   // var stdin = process.openStdin();
+//   // stdin.addListener("data", function(d) {
+//   //     ws.send(d.toString().trim())
+//   // });
+// });
