284cce46bcffd2e1ed06ce8b4342971578865340
LesMiserables1 <liemandre20@gmail.com>
2021-03-23 21:08:14 +0700

    fix websocket

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
+    ws.send(message)
     console.log('received: %s', message);
   });
-  var stdin = process.openStdin();
-  stdin.addListener("data", function(d) {
-      ws.send(d.toString().trim())
-  });
+  // var stdin = process.openStdin();
+  // stdin.addListener("data", function(d) {
+  //     ws.send(d.toString().trim())
+  // });
 });
+
