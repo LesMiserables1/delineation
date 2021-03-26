1a9cacc927fbf302fc884464ea37a5ef9e8fdfd5
LesMiserables1 <liemandre20@gmail.com>
2021-03-26 12:01:31 +0700

    update be

--- backend/client.js
+++ backend/client.js
@@ -1,11 +1,10 @@
-const io = require('socket.io-client')("ws://localhost:8080");
-io.on("connect", () => {
-  // // either with send()
-  io.send("Hello!")
-  io.on('message',(msg)=>{
-    console.log(msg)
-  })
-  // // or with emit() and custom event names
-  // socket.emit("salutations", "Hello!", { "mr": "john" }, Uint8Array.from([1, 2, 3, 4]));
+const WebSocket = require('ws');
+
+const ws = new WebSocket("wss://ws-pjpb.herokuapp.com/");
+ws.on('open', function open() {
+  ws.send('something');
 });
 
+ws.on('message', function incoming(data) {
+  console.log(`received: ${data}`);
+});
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-const WebSocket = require('ws');
+const {Server} = require('ws');
+const express = require('express')
 
 const PORT = process.env.PORT||3000
-const wss = new WebSocket.Server({ port: PORT });
+const server = express()
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
 
 wss.on('connection', function connection(ws) {
+  console.log('client connected')
   ws.on('message', function incoming(message) {
     ws.send(message)
   });
