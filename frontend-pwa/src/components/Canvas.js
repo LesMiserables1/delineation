c39a8d466a372a866374241dd173656e982c7b22
LesMiserables1 <liemandre20@gmail.com>
2021-05-18 19:55:14 +0700

    integrate websocket

--- frontend-pwa/src/components/Canvas.js
+++ frontend-pwa/src/components/Canvas.js
@@ -2,11 +2,12 @@ import React, { useEffect, useRef } from 'react'
 
 const Canvas = (props) => {
     const canvasRef = useRef(null)
+	var socket = new WebSocket('wss://ws-pjpb.herokuapp.com/');
   
-    const draw = ctx => {
+    const draw = (ctx,counter) => {
         ctx.fillStyle = '#000000'
         ctx.beginPath()
-        ctx.arc(50, 100, 20, 0, 2*Math.PI)
+        ctx.arc(50*counter, 100, 20, 0, 2*Math.PI)
         ctx.fill()
     }
     
@@ -14,13 +15,23 @@ const Canvas = (props) => {
         
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
-        
+        let counter = 0
         //Our draw come here
-        draw(context)
+        socket.onopen = () => socket.send(new Date().toGMTString());
+        
+        socket.onmessage = ({data}) => {
+            counter++
+            draw(context,counter)
+			console.log('Data Masuk', data)
+        } 
+
     }, [draw])
 
     return (
-        <canvas ref={canvasRef} {...props}/>
+        <>
+            <canvas ref={canvasRef} {...props}/>
+            <button onClick={() => socket.send('halo')}>Klik</button>        
+        </>
     )
 }
 
