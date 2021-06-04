b0535ef86f7d4c614dd2b5cff84821c3f6309a41
Dika Permana Putra <44795264+dikapermanaputra@users.noreply.github.com>
2021-06-05 04:17:04 +0700

    Update Room.js

--- frontend-pwa/src/pages/Room.js
+++ frontend-pwa/src/pages/Room.js
@@ -6,10 +6,10 @@ const Room = () => {
     
     const socket = new WebSocket('ws://localhost:3001');
     return (
-        <view
+        <>
             <Header text="delineation" />
             <Canvas height={500} width={500} socket={socket} />
-        <view/>
+        </>
     )
 }
 
