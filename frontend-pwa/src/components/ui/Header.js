fc9f269417642057e36ff95de870be60ecd8a844
Dika Permana Putra <44795264+dikapermanaputra@users.noreply.github.com>
2021-06-05 13:58:47 +0700

    Update Header.js

--- frontend-pwa/src/components/ui/Header.js
+++ frontend-pwa/src/components/ui/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Header = (props) => {
+const Header = () => {
     return (
-        <h3 className="text-xl md:text-2xl font-bold uppercase text-orange">{props.text}</h3>
+        <div className="header">
+            <p> <b>DELINEATION </b> </p>
+        </div>
     )
 }
 
