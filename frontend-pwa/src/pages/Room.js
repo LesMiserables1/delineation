4076e0c0c26736e6377a314d43f5676fe31547ab
Dika Permana Putra <44795264+dikapermanaputra@users.noreply.github.com>
2021-06-05 13:59:45 +0700

    Update Room.js

--- frontend-pwa/src/pages/Room.js
+++ frontend-pwa/src/pages/Room.js
@@ -1,27 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from '../components/ui/Header';
 import Canvas from '../components/Canvas';
-import { useSelector } from 'react-redux';
 
-const Room = (props) => {
-    
+const Room = () => {
+
     const socket = new WebSocket('ws://localhost:3001');
-    const {auth: {isLoggedIn}} = useSelector(state => state);
-    
-    useEffect(() => {
-        const roomNumber = localStorage.getItem('roomNumber')
-        if(!isLoggedIn) {
-            props.history.push('/login')
-        }
-        else if(!roomNumber) {
-            props.history.push('/')
-        }
-    }, [])
-    
     return (
         <>
-            <Header text="delineation" />
-            <Canvas height={500} width={500} socket={socket} />
+            <Header text="DELINEATION" />
+            <br></br>
+            <Canvas height={450} width={500} socket={socket} />
         </>
     )
 }
